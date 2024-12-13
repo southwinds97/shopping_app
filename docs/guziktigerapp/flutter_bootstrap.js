8b3e5fe551f8b9faa256e3b3a98394a1306a15e7
southwinds97 <southwinds970521@gmail.com>
2024-12-13 10:12:17 +0900

    Deploy Flutter web app to GitHub Pages under guzikTigerApp

--- docs/guziktigerapp/flutter_bootstrap.js
+++ docs/guziktigerapp/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"83bacfc52569459a4a654727cad2546820cb0d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "693598975"
+    serviceWorkerVersion: "4216814170"
   }
 });
--- docs/guziktigerapp/flutter_service_worker.js
+++ docs/guziktigerapp/flutter_service_worker.js
@@ -495,9 +495,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "f8dfb0cd0229c925fda2138a43322ff3
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "ec8340b0598e31fdc5b9192811f2f276",
-"index.html": "b8b478e4c38abdd3d26147eff9f3ebb2",
-"/": "b8b478e4c38abdd3d26147eff9f3ebb2",
+"flutter_bootstrap.js": "8f406db69d47f402431ef7f2d6c2b44c",
+"index.html": "b54a21bf5acd47cee88a6c9c3c86ead7",
+"/": "b54a21bf5acd47cee88a6c9c3c86ead7",
 "main.dart.js": "3f17d7360054a2ef123f60aea7fad4fe",
 "manifest.json": "15bf326bb7ac3333fff9eb9deef31f80",
 "version.json": "d4c1d7b3ce13283faf4a9860753d9952"};
