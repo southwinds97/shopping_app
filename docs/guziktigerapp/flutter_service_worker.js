76a34dd90da299f58d286c60fd59a8029aa89681
southwinds97 <southwinds970521@gmail.com>
2024-12-13 09:43:10 +0900

    Deploy Flutter web app to GitHub Pages under guzikTigerApp

--- docs/guziktigerapp/flutter_service_worker.js
+++ docs/guziktigerapp/flutter_service_worker.js
@@ -495,10 +495,11 @@ const RESOURCES = {"assets/AssetManifest.bin": "f8dfb0cd0229c925fda2138a43322ff3
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "d24b587877b829cc3b99ab0a87b37bef",
-"index.html": "29f07664af05897fd8807e362ab788b4",
-"/": "29f07664af05897fd8807e362ab788b4",
+"flutter_bootstrap.js": "d2f28383ed39e3ca52431a9b2a16e0a4",
+"index.html": "e8282721b354e4d440dc84053d6e903b",
+"/": "e8282721b354e4d440dc84053d6e903b",
 "main.dart.js": "3f17d7360054a2ef123f60aea7fad4fe",
+"manifest.json": "3cdc3aa0ebcd0faf08243017ca3c688d",
 "version.json": "d4c1d7b3ce13283faf4a9860753d9952"};
 // The application shell files that are downloaded before a service worker can
 // start.
